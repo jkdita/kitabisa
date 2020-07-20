8a66235573388b98d32fd5aea9efe07555af33a4
Jaka Ardita <jakaardita@gmail.com>
2020-07-21 00:03:07 +0700

    add more test case

--- main_test.go
+++ main_test.go
@@ -5,27 +5,37 @@ import (
 	"testing"
 )
 
-func TestFibonacci(t *testing.T) {
+func TestSum(t *testing.T) {
 	type args struct {
-		input int
+		x int
+		y int
 	}
-	var tests = []struct {
+	tests := []struct {
 		name string
 		args args
-		want []int
+		want int
 	}{
 		{
 			name: "success",
 			args: args{
-				4,
+				x: 1,
+				y: 2,
 			},
-			want: []int{0,1,1,2},
+			want: 3,
+		},
+		{
+			name: "success",
+			args: args{
+				x: 10,
+				y: 90,
+			},
+			want: 100,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Fibonacci(tt.args.input); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Fibonacci() = %v, want %v", got, tt.want)
+			if got := Sum(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Sum() = %v, want %v", got, tt.want)
 			}
 		})
 	}
@@ -48,6 +58,14 @@ func TestMultiply(t *testing.T) {
 				y: 3,
 			},
 			want: 6,
+		},
+		{
+			name: "success",
+			args: args{
+				x: 4,
+				y: 4,
+			},
+			want: 16,
 		},
 	}
 	for _, tt := range tests {
@@ -73,7 +91,14 @@ func TestPrime(t *testing.T) {
 			args: args{
 				4,
 			},
-			want: []int{2,3,5,7},
+			want: []int{2, 3, 5, 7},
+		},
+		{
+			name: "success",
+			args: args{
+				9,
+			},
+			want: []int{2, 3, 5, 7, 11, 13, 17, 19, 23},
 		},
 	}
 	for _, tt := range tests {
@@ -85,29 +110,34 @@ func TestPrime(t *testing.T) {
 	}
 }
 
-func TestSum(t *testing.T) {
+func TestFibonacci(t *testing.T) {
 	type args struct {
-		x int
-		y int
+		input int
 	}
-	tests := []struct {
+	var tests = []struct {
 		name string
 		args args
-		want int
+		want []int
 	}{
 		{
 			name: "success",
 			args: args{
-				x: 1,
-				y: 2,
+				4,
 			},
-			want: 3,
+			want: []int{0, 1, 1, 2},
+		},
+		{
+			name: "success",
+			args: args{
+				10,
+			},
+			want: []int{0, 1, 1, 2, 3, 5, 8, 13, 21, 34},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Sum(tt.args.x, tt.args.y); got != tt.want {
-				t.Errorf("Sum() = %v, want %v", got, tt.want)
+			if got := Fibonacci(tt.args.input); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Fibonacci() = %v, want %v", got, tt.want)
 			}
 		})
 	}
